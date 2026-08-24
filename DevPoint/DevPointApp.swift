4cf65abda87f308aa7fc8d23bb646f6a632f89bf
Dimitris Chatzigeorgiou <dimitris.chatzi@proton.me>
2026-08-24 19:14:54 +0300

    Initial Commit

---
+++ DevPoint/DevPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevPointApp.swift
+//  DevPoint
+//
+//  Created by mitz on 24/8/26.
+//
+
+import SwiftUI
+
+@main
+struct DevPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
